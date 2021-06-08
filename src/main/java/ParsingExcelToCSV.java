b3c9e5e6cb7d1f60759c2d8a22dd944dd18ad5d9
MirjamWeirer <mirjam.weirer@gmx.at>
2021-06-08 10:38:53 +0200

    Changes by Landa

--- src/main/java/ParsingExcelToCSV.java
+++ src/main/java/ParsingExcelToCSV.java
@@ -13,6 +13,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ParsingExcelToCSV {
+    private static final Logger log = LoggerFactory.getLogger(ParsingExcelToCSV.class);
 
     public static void main(String[] args) {
 
