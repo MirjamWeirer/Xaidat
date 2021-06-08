c254be7bdf4613e3e1c4bb6496c8596c8712e459
MirjamWeirer <mirjam.weirer@gmx.at>
2021-06-08 11:31:43 +0200

    ReadFormURL an InputStreamCSV

--- src/main/java/ReadFormURL.java
+++ src/main/java/ReadFormURL.java
@@ -1,7 +1,6 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ProxySelector;
@@ -14,32 +13,32 @@ import java.net.http.HttpResponse;
 
 public class ReadFormURL {
     private static final Logger log = LoggerFactory.getLogger(ReadFormURL.class);
-
-
-    public static void main(String[] args) {
-
-        File getCSVFile = new File("C:\\Users\\mirja\\Xaidat\\timeline-bundeslaendermeldungen.csv");
-
-//        try {
-//            Reader in = new FileReader(getCSVFile);
-//            Iterable<CSVRecord> records = CSVFormat.EXCEL.withDelimiter(';').withHeader("Datum","BundeslandID","Bevölkerung","Name","GemeldeteImpfungenLaender","GemeldeteImpfungenLaenderPro100").parse(in);
-//            for (CSVRecord record : records) {
-//                String date = record.get("Datum");
-//                String stateID = record.get("BundeslandID");
-//                String population = record.get("Bevölkerung");
-//                String name = record.get("Name");
-//                String reportVaccinationsPerState = record.get("GemeldeteImpfungenLaender");
-//                String reportVaccinationsPerStatePer100 = record.get("GemeldeteImpfungenLaenderPro100");
-//                System.out.println(date + " "+stateID + " "+population+" "+name+" "+reportVaccinationsPerState+" "+reportVaccinationsPerStatePer100);
 //
-//            }
-//            in.close();
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-    }
+//
+//    public static void main(String[] args) {
+//
+//        File getCSVFile = new File("C:\\Users\\mirja\\Xaidat\\timeline-bundeslaendermeldungen.csv");
+//
+////        try {
+////            Reader in = new FileReader(getCSVFile);
+////            Iterable<CSVRecord> records = CSVFormat.EXCEL.withDelimiter(';').withHeader("Datum","BundeslandID","Bevölkerung","Name","GemeldeteImpfungenLaender","GemeldeteImpfungenLaenderPro100").parse(in);
+////            for (CSVRecord record : records) {
+////                String date = record.get("Datum");
+////                String stateID = record.get("BundeslandID");
+////                String population = record.get("Bevölkerung");
+////                String name = record.get("Name");
+////                String reportVaccinationsPerState = record.get("GemeldeteImpfungenLaender");
+////                String reportVaccinationsPerStatePer100 = record.get("GemeldeteImpfungenLaenderPro100");
+////                System.out.println(date + " "+stateID + " "+population+" "+name+" "+reportVaccinationsPerState+" "+reportVaccinationsPerStatePer100);
+////
+////            }
+////            in.close();
+////        } catch (FileNotFoundException e) {
+////            e.printStackTrace();
+////        } catch (IOException e) {
+////            e.printStackTrace();
+////        }
+//    }
     public HttpResponse<InputStream> http(){
         HttpResponse<InputStream> response = null;
         try {
